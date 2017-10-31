a453c7e96ad1d4c74493176b122dac87b2e10951
webmurkser <webmurkser@gmail.com>
2017-10-31 15:29:35 +0100

    Initial Commit

---
+++ githubtest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  githubtest
+//
+//  Created by Arnold on 31.10.17.
+//  Copyright © 2017 webmurkser. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
